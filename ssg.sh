5b72e41c880f191166b8124e0c7ea7bebd9caf6e
MateusPitura <mateuspitura@gmail.com>
2026-04-01 22:23:48 -0300

    feat: updated ssg script

--- ssg.sh
+++ ssg.sh
@@ -8,3 +8,10 @@ rm -rf httrack/
 httrack http://localhost:8080 -O httrack "+*.localhost:8080/*" -v
 
 mv httrack/localhost_8080 dist
+
+docker cp wp_app:/var/www/html/wp-content/plugins/elementor/assets/js/. ./dist/wp-content/plugins/elementor/assets/js
+
+find dist/ -type f -exec sed -i 's|localhost:8080|saas.mateuspitura.com|g' {} +
+find dist/ -type f ! -name "*.svg" -exec sed -i 's|http:|https:|g' {} +
+
+cp public/CNAME dist/CNAME
